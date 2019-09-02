2a742f2c40ae962843bec180e56d9a905cdab988
DanielM3388 <israelibit@gmail.com>
2019-09-02 18:53:36 +0300

    something like that, remides me of PolynomialFunction

--- src/main/java/tracer/Profile.java
+++ src/main/java/tracer/Profile.java
@@ -7,16 +7,22 @@ public abstract class Profile {
     private final double initialVelocity;
     private final MotionParameters max;
     private final Time duration;
+    private final Time startTime;
 
-    public Profile(double startDistance, double startVelocity, MotionParameters max) {
-        this.initialDistance = startDistance;
-        this.initialVelocity = startVelocity;
+    public Profile(double initialDistance, double initialVelocity, MotionParameters max, Time startTime) {
+        this.initialDistance = initialDistance;
+        this.initialVelocity = initialVelocity;
         this.max = max;
 
-        duration = calcDuration(startVelocity, max);
+        duration = calcDuration(max);
+        this.startTime = startTime;
     }
 
-    protected abstract Time calcDuration(double startVelocity, MotionParameters max);
+    protected abstract Time calcDuration(MotionParameters max);
+
+    public boolean isCorresponding(Time currentTime) {
+        return currentTime.after(startTime) && currentTime.before(startTime.add(duration));
+    }
 
     public double getMaxVelocity() {
         return max.getVelocity();
@@ -42,6 +48,33 @@ public abstract class Profile {
         return duration;
     }
 
-    public abstract double velocityAt(Time time);
-    public abstract double distanceAt(Time time);
+    private double getRelativeTimeSeconds(Time currentTime) {
+        return currentTime.sub(startTime).valueAsMillis() / 1000.0;
+    }
+
+    public double getLength() {
+        return distanceAt(duration);
+    }
+
+    public double getFinalVelocity() {
+        return velocityAt(duration);
+    }
+
+    public double velocityAt(Time currentTime) {
+        return relativeVelocityAt(currentTime) + initialVelocity;
+    }
+
+    public double distanceAt(Time currentTime) {
+        return relativeDistanceAt(currentTime) + initialDistance;
+    }
+
+    public double relativeVelocityAt(Time currentTime) {
+        return relativeVelocityAt(getRelativeTimeSeconds(currentTime));
+    }
+    public double relativeDistanceAt(Time currentTime) {
+        return relativeDistanceAt(getRelativeTimeSeconds(currentTime));
+    }
+
+    public abstract double relativeVelocityAt(double t);
+    public abstract double relativeDistanceAt(double t);
 }
