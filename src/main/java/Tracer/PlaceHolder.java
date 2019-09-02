a5f120f8004be7bdaa50ecb3d3e93799ef56dbf8
DanielM3388 <israelibit@gmail.com>
2019-09-02 06:43:06 +0300

    maaan it's huge, Need to wrap it somehow

--- src/main/java/Tracer/PlaceHolder.java
+++ src/main/java/Tracer/PlaceHolder.java
@@ -4,9 +4,37 @@ import com.flash3388.flashlib.time.Time;
 
 public class PlaceHolder {
     private final Trajectory trajectory;
+    private final MotionParameters maxMotionParameters;
+    private final Time startOfStartSCurve;
+    private final Time startOfEndSCurve;
+    private final Time startOfConstantVelocity;
+    private final Time startOfConcave;
+    private final Time startOfLinear;
+    private final Time startOfConvex;
 
-    public PlaceHolder(Trajectory trajectory) {
+    public PlaceHolder(Trajectory trajectory, MotionParameters maxMotionParameters) {
         this.trajectory = trajectory;
+        this.maxMotionParameters = maxMotionParameters;
+
+        startOfConcave = Time.seconds(0.0);
+        startOfLinear = calcLinearStart(maxMotionParameters);
+        startOfConvex = calcConvexStart(maxMotionParameters);
+
+        startOfStartSCurve = Time.seconds(0.0);
+        startOfConstantVelocity = startOfLinear.add(startOfConvex);
+        startOfEndSCurve = startOfStartSCurve.add(calcEndSCurveLength(maxMotionParameters));
+    }
+
+    private Time calcLinearStart(MotionParameters maxMotionParameters) {
+        return null;
+    }
+
+    private Time calcConvexStart(MotionParameters maxMotionParameters) {
+        return null;
+    }
+
+    private Time calcEndSCurveLength(MotionParameters maxMotionParameters) {
+        return null;
     }
 
     public double VelocityAt(Time time) {
@@ -18,6 +46,66 @@ public class PlaceHolder {
     }
 
     public double angleAt(Time time) {
+        return 0.0;
+    }
+
+    private double velocityAtStartSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double velocityAtEndSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double velocityAtSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double velocityAtSCurve(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double velocityAtConvex(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double velocityAtLinear(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double velocityAtConcave(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double distanceAtStartSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double distanceAtEndSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double distanceAtSCurve(Time currentTime) {
+        return 0.0;
+    }
+
+    private double distanceAtSCurve(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double distanceAtConvex(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double distanceAtLinear(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double distanceAtConcave(Time time, State startState, State endState) {
+        return 0.0;
+    }
+
+    private double distanceAtConstantVelocity(Time time) {
         return 0.0;
     }
 }
--- src/main/java/Tracer/State.java
+++ src/main/java/Tracer/State.java
@@ -1,26 +1,12 @@
 package Tracer;
 
-import com.flash3388.flashlib.time.Time;
-
 public class State {
-    private final Time time;
-    private final Position position;
     private final MotionParameters motionParameters;
     private final double passedDistance;
 
-    public State(Time time, double x, double y, double heading, double velocity, double acceleration, double jerk, double passedDistance) {
-        this.time = new Time(time.value(), time.unit());
-        position = new Position(x, y, heading);
-        motionParameters = new MotionParameters(velocity, acceleration, jerk);
+    public State(MotionParameters motionParameters, double passedDistance) {
+        this.motionParameters = motionParameters;
         this.passedDistance = passedDistance;
-    }
-
-    public Time getTime() {
-        return time;
-    }
-
-    public Position getPosition() {
-        return position;
     }
 
     public MotionParameters getMotionParameters() {
