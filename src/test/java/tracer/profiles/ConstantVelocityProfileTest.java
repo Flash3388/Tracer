66f3f6882c44de7c3772c12251362bc87acd391f
DanielTheCUPMaker <danielmkhlv@gmail.com>
2019-09-15 09:07:20 +0300

    now added profile exception tests

--- src/test/java/tracer/profiles/ConstantVelocityProfileTest.java
+++ src/test/java/tracer/profiles/ConstantVelocityProfileTest.java
@@ -30,4 +30,82 @@ public class ConstantVelocityProfileTest {
 
         PROFILE.velocityAt(END_TIME.add(Time.seconds(0.1)));
     }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void distanceAt_forTimeBeforeStart_throwsException()    {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.distanceAt(INITIAL_TIME.sub(Time.seconds(0.1)));
+    }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void distanceAt_forTimeAfterEnd_throwsException() {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.distanceAt(END_TIME.add(Time.seconds(0.1)));
+    }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void accelerationAt_forTimeBeforeStart_throwsException()    {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.accelerationAt(INITIAL_TIME.sub(Time.seconds(0.1)));
+    }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void accelerationAt_forTimeAfterEnd_throwsException() {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.accelerationAt(END_TIME.add(Time.seconds(0.1)));
+    }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void jerkAt_forTimeBeforeStart_throwsException()    {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.jerkAt(INITIAL_TIME.sub(Time.seconds(0.1)));
+    }
+
+    @Test(expected = OutsideOfTimeBoundsException.class)
+    public void jerkAt_forTimeAfterEnd_throwsException() {
+        final double INITIAL_DISTANCE = 10;
+        final double INITIAL_VELOCITY = 5;
+
+        final Time INITIAL_TIME = Time.seconds(1);
+        final Time END_TIME = Time.seconds(2);
+
+        final ConstantVelocityProfile PROFILE = new ConstantVelocityProfile(INITIAL_DISTANCE, INITIAL_VELOCITY, INITIAL_TIME, END_TIME.sub(INITIAL_TIME));
+
+        PROFILE.jerkAt(END_TIME.add(Time.seconds(0.1)));
+    }
 }
