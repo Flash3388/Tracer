e8e6a1d9301f56ac0c585867dea00d7f56753105
DanielTheCUPMaker <danielmkhlv@gmail.com>
2020-01-01 18:31:48 +0200

    moved to junit5

--- tracerj/src/test/java/tracer/profiles/ConvexProfileTest.java
+++ tracerj/src/test/java/tracer/profiles/ConvexProfileTest.java
@@ -1,10 +1,10 @@
 package tracer.profiles;
 
 import com.flash3388.flashlib.time.Time;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import tracer.motion.MotionParameters;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConvexProfileTest {
     private final static double DEF_DELTA = 0.001;
